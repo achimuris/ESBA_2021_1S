28d7ac4e36eb9dc5e9eef77a15be19dbde64e2ef
Andrés Chimuris <38164432+achimuris@users.noreply.github.com>
2021-05-11 23:08:12 -0300

    Clase 08

---
+++ Clase08/3_Capas/BLL/Cliente.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLL
+{
+    public class Cliente
+    {
+        private string _nombre;
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+
+        private string _apellido;
+
+        public string Apellido
+        {
+            get { return _apellido; }
+            set { _apellido = value; }
+        }
+
+
+        private int _codigoDeCliente;
+        public int Codigo
+        {
+            get { return _codigoDeCliente; }
+            set { _codigoDeCliente = value; }
+        }
+
+        private TipoDeCliente _tipoDeCliente;
+        public TipoDeCliente TipoDeCliente
+        {
+            get { return _tipoDeCliente; }
+            set { _tipoDeCliente = value; }
+        }
+    }
+}
