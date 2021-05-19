f42eb4f29edb9469311d3b48a0defcc24b00988f
Andrés Chimuris <38164432+achimuris@users.noreply.github.com>
2021-05-18 21:36:17 -0300

    Clase 09

---
+++ Clase09/3_Capas/BLL/TipoDeCliente.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace BLL
+{
+    public class TipoDeCliente
+    {
+        private int _codigo;
+
+        public int Codigo
+        {
+            get { return _codigo; }
+            set { _codigo = value; }
+        }
+
+        private string _descripcion;
+
+        public string Descripcion
+        {
+            get { return _descripcion; }
+            set { _descripcion = value; }
+        }
+
+
+        public DataTable DevolverTipos()
+        {
+            DAL.TipoDeCliente objDAL = new DAL.TipoDeCliente();
+
+            return objDAL.GetTipos();
+        }
+
+        public int CrearTipo()
+        {
+
+            DAL.TipoDeCliente objDAL = new DAL.TipoDeCliente();
+
+            return objDAL.CrearTipo(this.Descripcion);
+        }
+
+    }
+}
